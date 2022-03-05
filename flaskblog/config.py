c64993103401cbd70c61fbcf81fc896f638ced6c
Robert Such <85602363+Robert-Such@users.noreply.github.com>
2022-03-04 20:26:43 -0500

    Full Working Tutorial

---
+++ flaskblog/config.py
@@ -0,0 +1,17 @@
+"""App configuration."""
+from os import environ, path
+
+from dotenv import load_dotenv
+
+basedir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(basedir, ".env"))
+
+
+class Config:
+    SECRET_KEY = environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = environ.get('MS2_SQLALCHEMY_DATABASE_URI')
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = environ.get('EMAIL_USER')
+    MAIL_PASSWORD = environ.get('EMAIL_PASS')
