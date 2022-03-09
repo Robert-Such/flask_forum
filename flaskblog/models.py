74af0e358bd85cc0da5ba91b28b9a659a093bc71
Robert Such <85602363+Robert-Such@users.noreply.github.com>
2022-03-09 02:35:05 -0500

    added default avatars for new accounts based on initials in username.  this fixes #2

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -3,6 +3,7 @@ from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app
 from flaskblog import db, login_manager
 from flask_login import UserMixin
+import avinit
 
 
 @login_manager.user_loader
@@ -10,15 +11,23 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 
+def default_avatar(context):
+    name = context.get_current_parameters()['username']
+    avinit.get_png_avatar(name, output_file=f"flaskblog/static/profile_pics/{name}.png")
+    return f"{name}.png"
+
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    image_file = db.Column(db.String(50), nullable=False, default=default_avatar)
     password = db.Column(db.String(60), nullable=False)
     date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     posts = db.relationship('Post', backref='author', lazy=True)
     comments = db.relationship('Comment', backref='author', lazy=True)
+
+
 
     def get_reset_token(self, expires_sec=1800):
         s = Serializer(current_app.config['SECRET_KEY'], expires_sec)
@@ -32,6 +41,8 @@ class User(db.Model, UserMixin):
         except:
             return None
         return User.query.get(user_id)
+
+
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}', '{self.date_created}')"
